b91796742584c62cfd43e4e4826c69c73747f809
rajat <rajat241994@gmail.com>
2021-02-01 09:51:05 +0530

    Add python lsp support

--- config.el
+++ config.el
@@ -65,3 +65,9 @@
   :ensure t
   :config
   (super-save-mode +1))
+(use-package lsp-python-ms
+  :ensure t
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-python-ms)
+                          (lsp))))  ; or lsp-deferred
--- packages.el
+++ packages.el
@@ -11,6 +11,7 @@
 (package! tree-sitter)
 (package! tree-sitter-langs)
 (package! super-save)
+(package! lsp-python-ms)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
